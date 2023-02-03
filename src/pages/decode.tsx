1892337b0fe9f9b3bde2deda9f021cf3adec64cd
LvarunL <varunvyas312@gmail.com>
2023-02-04 04:06:55 +0530

    trying1

--- src/pages/decode.tsx
+++ src/pages/decode.tsx
@@ -1,7 +1,40 @@
+import { doc, getDoc } from "firebase/firestore";
+import { useState } from "react";
+import { db } from "../config/firebase";
+
+
 export const Decode = () => {
+    const [messageID, setMessageID] = useState("");
+    const [pw, setPW] = useState("");
+
+    const changeMessage=(data: any)=>{
+        setMessageID(data.target.value);
+    }
+
+    const addPW=(data:any)=>{
+        setPW(data.target.value);
+    }
+
+    const searchID=async()=>{
+        const docRef = doc(db,"messages" ,messageID);
+        const docSnap = await getDoc(docRef);
+        const flag = "";
+        console.log(docSnap.data);
+
+        return (
+            <div> 
+                    
+            </div>
+        )
+    }
+
     return (
+
         <div className="pages">
-            Decode page
+            <input type="text" onChange={changeMessage} placeholder="enter secret message..."/>
+            <p> {messageID}</p>
+            <input type="password" onChange={addPW} placeholder="enter password..."/>
+            <button> search </button>
         </div>
     )
 }
