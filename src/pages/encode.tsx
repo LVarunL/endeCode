0980e727e9d842f7b2b30eeb2822dc52c6b25a94
dhruvi188 <dhruvigohel2312@gmail.com>
2023-02-04 01:35:04 +0530

    trying(not done) firebase input

--- src/pages/encode.tsx
+++ src/pages/encode.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
+
+interface CreateFormData{
+    message: string;
+}
 export const Encode = () => {
+    const [messageToAdd,setMessageToAdd] = useState("");
+    const getmessage=(data:any)=>{
+        setMessageToAdd(data.target.value);
+    }
+    const messageref = collection(db, "messages"); 
+
+    const Storemessage = async()=>{
+        await addDoc(collection(messageref,"messsages"), {
+            message: messageToAdd
+        })
+    }
+        
     return (
+        <form onSubmit={()=>Storemessage}>
         <div className="encode">
         <div className="encodeInput">
-            <textarea placeholder="enter your message here..." />
+            <textarea placeholder="enter your message here..." onChange={getmessage}/>
+            <h2>{messageToAdd}</h2>
+            {/* <button onClick={()=> Storemessage}>enter</button> */}
+            <input type="submit"/>
         </div>
         </div>
+        </form>
     )
 }
